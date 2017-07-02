7acae3a4a1fe485c4e3ff2ef73666f2d6e0e3bd6
Hayson <heyson@sina.cn>
2017-07-03 01:16:59 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :jobs
-  namespace :admin do
-  resources :jobs
-end
-  root 'jobs#index'
-
-
 
   namespace :admin do
       resources :jobs do
@@ -16,5 +9,11 @@ end
         end
       end
     end
+
+    resources :jobs do
+      resources :resumes
+    end
+
+    root 'jobs#index'
 
 end
